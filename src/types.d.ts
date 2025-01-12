f955a74e8f9848c383394c6b7f0d48b3cc4a4013
Tobias Lins <me@tobi.sh>
2025-01-12 13:18:01 +0100

    testing

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,6 @@
-declare module "ws" {
-  class WebSocket extends globalThis.WebSocket {
-    constructor(url: string | URL, protocols?: string | string[]);
-  }
-  export { WebSocket };
-}
+// declare module "ws" {
+//   class WebSocket extends globalThis.WebSocket {
+//     constructor(url: string | URL, protocols?: string | string[]);
+//   }
+//   export { WebSocket };
+// }
