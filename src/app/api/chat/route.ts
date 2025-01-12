fc1ab2d14aa5bbe29e39eb290d98a0545f98518f
Tobias Lins <me@tobi.sh>
2025-01-12 14:08:49 +0100

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -44,7 +44,8 @@ export async function POST(req: Request) {
     try {
       console.log("Starting worker");
       const w = await startWorker({
-        syncServer: "wss://cloud.jazz.tools/?key=jazz-ai-chat",
+        syncServer:
+          "wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self",
         AccountSchema: WorkerAccount,
         accountID: process.env.JAZZ_WORKER_ACCOUNT,
         accountSecret: process.env.JAZZ_WORKER_SECRET,
