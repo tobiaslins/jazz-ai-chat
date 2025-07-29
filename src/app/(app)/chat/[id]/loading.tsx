dd9e75ad51fcef42c6abf391839102795dd8cd93
Tobias Lins <me@tobi.sh>
2025-07-29 18:15:05 +0200

    Create loading.tsx

---
+++ src/app/(app)/chat/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function ChatLoading() {
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+                <div className="w-full h-10 bg-gray-200 rounded-md"></div>
+            </div>
+        </div>
+    )
+}
