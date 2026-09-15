74ffc4cb5f1cc14473f876eb896be8ab53f9255e
Tobias Lins <me@tobi.sh>
2026-09-15 20:51:05 +0400

    Reduce Jazz NAPI server trace size

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ export default withJazz(
         "./node_modules/.pnpm/@garden-co+jazz-napi-linux-x64-gnu@2.0.0-alpha.54/node_modules/@garden-co/jazz-napi-linux-x64-gnu/**/*",
       ],
     },
+    outputFileTracingExcludes: {
+      "/api/chat": [
+        "./node_modules/.pnpm/jazz-napi@2.0.0-alpha.54/node_modules/jazz-napi/jazz-napi.darwin-*.node",
+        "./node_modules/.pnpm/jazz-napi@2.0.0-alpha.54/node_modules/jazz-napi/jazz-napi.win32-*.node",
+        "./node_modules/jazz-napi/jazz-napi.darwin-*.node",
+        "./node_modules/jazz-napi/jazz-napi.win32-*.node",
+      ],
+    },
   },
   {
     server: false,
